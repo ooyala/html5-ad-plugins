9460dfd8be9fec3575029f95b17cf5d16df0a5d3
Peter Iliev <piliev@ooyala.com>
2015-10-29 17:26:30 -0700

    pbi-258 updating gulp file to package all 'require' files. Adding in new submodule that refers to our html5-common repo.  Moved vast ad manager out to this repo.

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,64 @@
 var gulp = require('gulp'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
-    buffer = require('vinyl-buffer'),
     gutil = require('gulp-util'),
-    sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
     shell = require('gulp-shell'),
     rename = require('gulp-rename');
+    _ = require('underscore');
+    listFiles = require('file-lister');
 
 var path = {
-  scripts: ['./js/*.js']
+  originalJs: ['./js/'],
+  standAloneJs: ['./tmp/*.js']
 };
 
 // Build All
-gulp.task('build', ['publish_min', "publish_debug"]);
+gulp.task('build', ['browserify', "publish_debug", "publish_min"]);
+
+gulp.task('browserify', function() {
+  debug = true;
+  var destDir = "./tmp";
+
+
+  var bundleThis = function(srcArray)
+  {
+    _.each(srcArray, function(sourceFile)
+    {
+      var bundle = browserify(sourceFile).bundle();
+      bundle.pipe(source(getFileNameFromPath(sourceFile)))
+            .pipe(gulp.dest(destDir));
+    });
+  };
+
+  listFiles(path.originalJs, function(error, files) {
+    if (error) {
+        console.log(error);
+    } else {
+      var filteredList = files.filter(_.bind(checkFileExtension,this,".js"));
+      bundleThis(filteredList);
+    }});
+
+});
+
+var checkFileExtension = function(extension, fileName)
+{
+    if (!fileName || fileName.length < extension.length)
+    {
+      return false;
+    }
+
+    return (fileName.lastIndexOf(extension) == fileName.length - extension.length);
+}
+
+var getFileNameFromPath = function(path)
+{
+  var start = path.lastIndexOf('/') + 1;
+  return path.substring(start);
+}
 
 gulp.task('publish_min', function() {
-  return gulp.src('js/*.js')
+  return gulp.src(path.standAloneJs)
     .pipe(uglify())
     .pipe(rename({
       extname: '.min.js'
@@ -30,7 +72,7 @@ gulp.task('publish_min', function() {
 });
 
 gulp.task('publish_debug', function() {
-  return gulp.src('js/*.js')
+  return gulp.src(path.standAloneJs)
     .on('error', gutil.log)
     .pipe(gulp.dest('./build/'));
 });
@@ -40,7 +82,7 @@ gulp.task('test', shell.task(['npm test']));
 
 // Initiate a watch
 gulp.task('watch', function() {
-  gulp.watch(path.scripts, ['publish_min', 'publish_debug']);
+  gulp.watch(path.scripts, ['browserify', 'publish_min', 'publish_debug']);
 });
 
 // The default task (called when you run `gulp` from cli)
