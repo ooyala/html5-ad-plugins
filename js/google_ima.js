56d520378cb78e81f296d2486794b449c4bb75fc
dchau <dchau@ooyala.com>
2015-12-01 15:51:16 -0800

    [PBI-1191] Change url parameter to null. Add additional commentary

--- js/google_ima.js
+++ js/google_ima.js
@@ -425,8 +425,9 @@ require("../html5-common/js/utils/utils.js");
           //displaying it, so just notify AMC that we are showing an overlay.
           else
           {
-            // raise WILL_PLAY_NONLINEAR_AD event and notify nonlinear ad started
-            _amc.sendURLToLoadAndPlayNonLinearAd(this.currentAMCAdPod, this.currentAMCAdPod.id, this.currentAMCAdPod.ad.tag_url);
+            // raise WILL_PLAY_NONLINEAR_AD event and alert AMC and player that a nonlinear ad is started.
+            // Nonlinear ad is rendered by IMA.
+            _amc.sendURLToLoadAndPlayNonLinearAd(this.currentAMCAdPod, this.currentAMCAdPod.id, null);
           }
         }
       };
