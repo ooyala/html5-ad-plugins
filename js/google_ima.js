819d1c6d5cea8b775fbb685e135b0e7ea18504b3
Alex Eng <aeng@ooyala.com>
2016-01-14 14:45:12 -0800

    [PBW-4052] -IMA to auto scale on size changes and init

--- js/google_ima.js
+++ js/google_ima.js
@@ -697,7 +697,7 @@ require("../html5-common/js/utils/utils.js");
 
             var w = _amc.ui.width;
             var h = _amc.ui.height;
-            _IMAAdsManager.init(w, h, google.ima.ViewMode.NORMAL);
+            _IMAAdsManager.init(google.ima.AdsRenderingSettings.AUTO_SCALE, google.ima.AdsRenderingSettings.AUTO_SCALE, google.ima.ViewMode.NORMAL);
             _IMAAdsManagerInitialized = true;
             if(this.vcPlayRequested)
             {
@@ -776,19 +776,7 @@ require("../html5-common/js/utils/utils.js");
         if (_IMAAdsManager)
         {
           var viewMode = this.isFullscreen ? google.ima.ViewMode.FULLSCREEN : google.ima.ViewMode.NORMAL;
-          if (this.currentIMAAd && !this.currentIMAAd.isLinear())
-          {
-            // For nonlinear ads, the ad slot can be adjusted at this time.
-            // In this example, we make the ad to be shown at the bottom
-            // of the slot. We also make the slot a bit shorter, so there is
-            // a padding at the bottom.
-            _IMAAdsManager.resize(_amc.ui.width, _amc.ui.height - OVERLAY_OFFSET, viewMode);
-          }
-          else
-          {
-            // For linear ad, set the size to the full video player size.
-            _IMAAdsManager.resize(_amc.ui.width, _amc.ui.height, viewMode);
-          }
+          _IMAAdsManager.resize(google.ima.AdsRenderingSettings.AUTO_SCALE, google.ima.AdsRenderingSettings.AUTO_SCALE, viewMode);
         }
       });
 
