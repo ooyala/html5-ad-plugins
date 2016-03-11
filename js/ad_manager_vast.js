13b4c3de72ea8cd4f58db88cebe32e4393c585c0
dchau <dchau@ooyala.com>
2016-03-11 14:21:36 -0800

    [PBI-1518] Add full adBreak object into adObject

--- js/ad_manager_vast.js
+++ js/ad_manager_vast.js
@@ -1855,34 +1855,34 @@ OO.Ads.manager(function(_, $) {
         time: 0,
         position_type: "t",
       };
-      if (!adBreak) {
-        return null;
-      }
-      if (adBreak.repeatAfter) {
-        adObject.repeatAfter = _convertTimeStampToSeconds(adBreak.repeatAfter) / 1000;
-      }
-      if (adBreak.timeOffset) {
-        switch(true) {
-          // case: "start"
-          case /^start$/.test(adBreak.timeOffset):
-            adObject.time = 0;
-            break;
-          // case: "end"
-          case /^end$/.test(adBreak.timeOffset):
-            adObject.time = (this.amc.movieDuration + 1) * 1000;
-            break;
-          // case: hh:mm:ss.mmm | hh:mm:ss
-          case /^\d{2}:\d{2}:\d{2}\.000$|^\d{2}:\d{2}:\d{2}$/.test(adBreak.timeOffset):
-            adObject.time = _convertTimeStampToSeconds(adBreak.timeOffset);
-            break;
-          // case: [0, 100]%
-          case /^\d{1,3}%$/.test(adBreak.timeOffset):
-            // TODO: test percentage > 100
-            adObject.time = _convertPercentToSeconds(adBreak.timeOffset);
-            break;
-          default:
-            OO.log("VAST, VMAP: No Matching 'timeOffset' Attribute format");
-            adObject = null;
+      if (adBreak) {
+        adObject.VMAPAdBreak = adBreak;
+        if (adBreak.repeatAfter) {
+          adObject.repeatAfter = _convertTimeStampToSeconds(adBreak.repeatAfter) / 1000;
+        }
+        if (adBreak.timeOffset) {
+          switch(true) {
+            // case: "start"
+            case /^start$/.test(adBreak.timeOffset):
+              adObject.time = 0;
+              break;
+            // case: "end"
+            case /^end$/.test(adBreak.timeOffset):
+              adObject.time = (this.amc.movieDuration + 1) * 1000;
+              break;
+            // case: hh:mm:ss.mmm | hh:mm:ss
+            case /^\d{2}:\d{2}:\d{2}\.000$|^\d{2}:\d{2}:\d{2}$/.test(adBreak.timeOffset):
+              adObject.time = _convertTimeStampToSeconds(adBreak.timeOffset);
+              break;
+            // case: [0, 100]%
+            case /^\d{1,3}%$/.test(adBreak.timeOffset):
+              // TODO: test percentage > 100
+              adObject.time = _convertPercentToSeconds(adBreak.timeOffset);
+              break;
+            default:
+              OO.log("VAST, VMAP: No Matching 'timeOffset' Attribute format");
+              adObject = null;
+          }
         }
       }
       return adObject;
