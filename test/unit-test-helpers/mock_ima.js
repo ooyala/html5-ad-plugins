208038a341233909fba2b43a9132b6a1ffd900bc
Alex Eng <aeng@ooyala.com>
2017-01-04 17:59:37 -0800

    [ad_error_fix] -removed AD_ERROR from AD_EVENT in the mock IMA implementation

--- test/unit-test-helpers/mock_ima.js
+++ test/unit-test-helpers/mock_ima.js
@@ -182,7 +182,6 @@ google =
         USER_CLOSE : "userClose",
         DURATION_CHANGE : "durationChange",
         CLICK : "click",
-        AD_ERROR : "adError",
         CONTENT_PAUSE_REQUESTED : "contentPauseRequested",
         CONTENT_RESUME_REQUESTED : "contentResumeRequested"
       }
