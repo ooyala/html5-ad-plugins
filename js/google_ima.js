e40ce492d5ae9f67b313551bdcfab5aa71985481
Aleksei Mohnatkin <42436929+ooyala-amohnatkin@users.noreply.github.com>
2019-05-08 15:55:24 +0300

    Revert "[PBW-7945] (#314)"
    This reverts commit 65d9b57e5d3320bfe8af192f2cf7f1bd85bb9bc2.

--- js/google_ima.js
+++ js/google_ima.js
@@ -1589,7 +1589,6 @@ require('../html5-common/js/utils/utils.js');
         // don't have ad object associated.
         const eventType = google.ima.AdEvent.Type;
         const ad = adEvent.getAd();
-
         // Retrieve the ad data as well.
         // Some events will not have ad data associated.
         const adData = adEvent.getAdData();
@@ -1604,9 +1603,7 @@ require('../html5-common/js/utils/utils.js');
             if (_amc.ui.useSingleVideoElement) {
               _amc.focusAdVideo();
             }
-            if (ad) {
-              this.currentMedia = ad.getMediaUrl();
-            }
+            this.currentMedia = ad.getMediaUrl();
             _resetUIContainerStyle();
             break;
           case eventType.STARTED:
@@ -1626,7 +1623,7 @@ require('../html5-common/js/utils/utils.js');
             }
 
             this.adPlaybackStarted = true;
-            if (ad && ad.isLinear()) {
+            if (ad.isLinear()) {
               _linearAdIsPlaying = true;
 
               if (!_amc.ui.useSingleVideoElement) {
@@ -1654,7 +1651,7 @@ require('../html5-common/js/utils/utils.js');
             _onSizeChanged();
             _tryStartAd();
             if (this.videoControllerWrapper) {
-              if (ad && ad.isLinear()) {
+              if (ad.isLinear()) {
                 this.videoControllerWrapper.raisePlayEvent();
               }
               this.videoControllerWrapper.raiseTimeUpdate(this.getCurrentTime(), this.getDuration());
@@ -1663,13 +1660,13 @@ require('../html5-common/js/utils/utils.js');
             // Non-linear ad rules or VMAP ads will not be started by _tryStartAd()
             // because there'll be no AMC ad pod. We start them here after the time update event
             // in order to prevent the progress bar from flashing
-            if (_usingAdRules && ad && !ad.isLinear()) {
+            if (_usingAdRules && !ad.isLinear()) {
               _startNonLinearAdRulesOverlay();
             }
             break;
           case eventType.RESUMED:
             if (this.videoControllerWrapper) {
-              if (ad && ad.isLinear()) {
+              if (ad.isLinear()) {
                 this.videoControllerWrapper.raisePlayEvent();
               }
             }
@@ -2138,10 +2135,8 @@ require('../html5-common/js/utils/utils.js');
           } else {
             _endCurrentAdPod(true);
           }
-        } else if (forceEndAdPod) {
-          _resetAdsState();
         }
-        
+
         _resetUIContainerStyle();
         this.currentIMAAd = null;
         this.adPlaybackStarted = false;
